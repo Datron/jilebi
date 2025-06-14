36d67eff24b048414369a4e05ecad89a67e3e065
datron <kartik54321@hotmail.com>
2025-06-14 17:56:24 +0530

    feat: plugin manifest parser working with rmcp types

--- crates/jilebi/src/main.rs
+++ crates/jilebi/src/main.rs
@@ -1,26 +1,27 @@
 use std::{fs, path::Path};
-mod server;
+// mod server;
 // use dosa::run_code;
 use jilebi_types::plugin::Manifest;
-use rmcp::{ServiceExt, transport::stdio};
-use server::JilebiMcpServer;
+// use rmcp::{ServiceExt, transport::stdio};
+// use server::JilebiMcpServer;
 
 #[tokio::main]
 async fn main() -> Result<(), String> {
     let toml_file_path =
         Path::new("/home/kartik/jilebi/examples/ts-simple-computer-use/manifest.toml");
     let manifest = fs::read_to_string(toml_file_path).expect("Manifest file not found");
-    let manifest = toml::from_str::<Manifest>(&manifest).expect("Could not parse toml file");
+    let manifest = toml::from_str::<toml::Value>(&manifest).expect("Could not parse toml file");
+	let manifest = Manifest::try_from(manifest)?;
 
-    // println!("{:#?}", manifest);
-    let mut server = JilebiMcpServer::new();
-    server.add(manifest).await;
+    println!("{:#?}", manifest);
+    // let mut server = JilebiMcpServer::new();
+    // server.add(manifest).await;
 
-    let service = server.serve(stdio()).await.map_err(|e| {
-        tracing::error!("Serving error: {:?}", e);
-        e.to_string()
-    })?;
-    service.waiting().await.map_err(|e| e.to_string())?;
+    // let service = server.serve(stdio()).await.map_err(|e| {
+    //     tracing::error!("Serving error: {:?}", e);
+    //     e.to_string()
+    // })?;
+    // service.waiting().await.map_err(|e| e.to_string())?;
     Ok(())
     // let code =
     //     fs::read_to_string("examples/ts-simple-computer-use/main.js").expect("File not found");
--- crates/jilebi_types/src/plugin.rs
+++ crates/jilebi_types/src/plugin.rs
@@ -2,7 +2,10 @@ use std::{borrow::Cow, collections::HashMap, sync::Arc};
 
 use derive_more::Deref;
 use either::Either::{self, Left, Right};
-use rmcp::model::{Prompt, RawResource, RawResourceTemplate, Resource, ResourceTemplate, Tool};
+use rmcp::model::{
+    Prompt, PromptArgument, PromptMessage, PromptMessageContent, PromptMessageRole, RawResource,
+    RawResourceTemplate, Resource, ResourceTemplate, Tool,
+};
 use serde::{Deserialize, Serialize};
 use toml::Table;
 
@@ -38,59 +41,51 @@ impl TryFrom<&toml::Value> for Resources {
     type Error = String;
 
     fn try_from(value: &toml::Value) -> Result<Self, Self::Error> {
-        match value {
-            toml::Value::Table(map) => {
-                let mut resource_map: HashMap<ResourceKey, JilebiResource> = HashMap::new();
-                for (key, table) in map.iter() {
-                    let Some(op_table) = table.as_table() else {
-                        // TODO: Add docs for formats
-                        return Err(format!(
-                            "Invalid format for resource definition {key}, please check the toml file. Refer the docs:"
-                        ));
-                    };
-                    let resource = if op_table.contains_key("uri_template") {
-                        Right(ResourceTemplate::new(
-                            RawResourceTemplate {
-                                uri_template: mandatory_extractor(
-                                    op_table,
-                                    key,
-                                    &"uri_template".to_string(),
-                                )?,
-                                name: mandatory_extractor(op_table, key, &"name".to_string())?,
-                                description: optional_extractor(
-                                    op_table,
-                                    &"description".to_string(),
-                                ),
-                                mime_type: optional_extractor(op_table, &"mime_type".to_string()),
-                            },
-                            None,
-                        ))
-                    } else {
-                        Left(Resource::new(
-                            RawResource {
-                                uri: mandatory_extractor(op_table, key, &"uri".to_string())?,
-                                name: mandatory_extractor(op_table, key, &"name".to_string())?,
-                                description: optional_extractor(
-                                    op_table,
-                                    &"description".to_string(),
-                                ),
-                                mime_type: optional_extractor(op_table, &"mime_type".to_string()),
-                                size: optional_extractor(op_table, &"".to_string())
-                                    .and_then(|s| s.parse::<u32>().ok()),
-                            },
-                            None,
-                        ))
-                    };
-                    let jilebi_resource = JilebiResource {
-                        resource,
-                        function: mandatory_extractor(op_table, key, &"function".to_string())?,
-                    };
-                    resource_map.insert(key.to_string(), jilebi_resource);
-                }
-                Ok(Self(resource_map))
-            }
-            _ => Err("Invalid format for Resources section".to_string()),
+        let map = value
+            .as_table()
+            .ok_or("Invalid format for Resources section".to_string())?;
+        let mut resource_map: HashMap<ResourceKey, JilebiResource> = HashMap::new();
+        for (key, table) in map.iter() {
+            let Some(op_table) = table.as_table() else {
+                // TODO: Add docs for formats
+                return Err(format!(
+                    "Invalid format for resource definition {key}, please check the toml file. Refer the docs:"
+                ));
+            };
+            let resource = if op_table.contains_key("uri_template") {
+                Right(ResourceTemplate::new(
+                    RawResourceTemplate {
+                        uri_template: mandatory_extractor(
+                            op_table,
+                            key,
+                            &"uri_template".to_string(),
+                        )?,
+                        name: mandatory_extractor(op_table, key, &"name".to_string())?,
+                        description: optional_extractor(op_table, &"description".to_string()),
+                        mime_type: optional_extractor(op_table, &"mime_type".to_string()),
+                    },
+                    None,
+                ))
+            } else {
+                Left(Resource::new(
+                    RawResource {
+                        uri: mandatory_extractor(op_table, key, &"uri".to_string())?,
+                        name: mandatory_extractor(op_table, key, &"name".to_string())?,
+                        description: optional_extractor(op_table, &"description".to_string()),
+                        mime_type: optional_extractor(op_table, &"mime_type".to_string()),
+                        size: optional_extractor(op_table, &"".to_string())
+                            .and_then(|s| s.parse::<u32>().ok()),
+                    },
+                    None,
+                ))
+            };
+            let jilebi_resource = JilebiResource {
+                resource,
+                function: mandatory_extractor(op_table, key, &"function".to_string())?,
+            };
+            resource_map.insert(key.to_string(), jilebi_resource);
         }
+        Ok(Self(resource_map))
     }
 }
 
@@ -103,71 +98,122 @@ pub struct JilebiTool {
 }
 
 #[derive(Debug, Clone, Serialize, Deserialize, Deref)]
-pub struct Tools(pub HashMap<ToolKey, Tool>);
+pub struct Tools(pub HashMap<ToolKey, JilebiTool>);
 
 impl TryFrom<&toml::Value> for Tools {
     type Error = String;
 
     fn try_from(value: &toml::Value) -> Result<Self, Self::Error> {
-        match value {
-            toml::Value::Table(map) => {
-                let mut tools_map: HashMap<ToolKey, Tool> = HashMap::new();
-                for (key, table) in map.iter() {
-                    let Some(op_table) = table.as_table() else {
-                        return Err(format!(
-                            "Invalid format for resource definition {key}, check the toml"
-                        ));
-                    };
-                    let schema = serde_json::json!(
-                        op_table
-                            .get("input_schema")
-                            .ok_or(format!("Missing field input_schema in resource {key}"))?
-                    )
-                    .as_object()
-                    .cloned()
-                    .ok_or(format!(
-                        "Invalid JSON format for the field input_schema in resource {key}"
-                    ))?;
-                    let tool = Tool {
-                        name: Cow::from(mandatory_extractor(op_table, key, &"name".to_string())?),
-                        description: optional_extractor(op_table, &"description".to_string())
-                            .map(Cow::from),
-                        input_schema: Arc::new(schema),
-                        annotations: None,
-                    };
-                    tools_map.insert(key.to_string(), tool);
-                }
-                Ok(Self(tools_map))
-            }
-            _ => Err("Invalid format for Tools section".to_string()),
+        let map = value
+            .as_table()
+            .ok_or("Invalid format for Tools section".to_string())?;
+        let mut tools_map: HashMap<ToolKey, JilebiTool> = HashMap::new();
+        for (key, table) in map.iter() {
+            let Some(op_table) = table.as_table() else {
+                return Err(format!(
+                    "Invalid format for tool definition {key}, check the toml"
+                ));
+            };
+            let schema = serde_json::json!(
+                op_table
+                    .get("input_schema")
+                    .ok_or(format!("Missing field input_schema in tool {key}"))?
+            )
+            .as_object()
+            .cloned()
+            .ok_or(format!(
+                "Invalid JSON format for the field input_schema in tool {key}"
+            ))?;
+            let tool = Tool {
+                name: Cow::from(mandatory_extractor(op_table, key, &"name".to_string())?),
+                description: optional_extractor(op_table, &"description".to_string())
+                    .map(Cow::from),
+                input_schema: Arc::new(schema),
+                annotations: None,
+            };
+            let jilebi_tool = JilebiTool {
+                tool,
+                function: mandatory_extractor(op_table, key, &"function".to_string())?,
+            };
+            tools_map.insert(key.to_string(), jilebi_tool);
         }
+        Ok(Self(tools_map))
     }
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct JilebiPrompt {
+    pub prompt: Prompt,
+    pub content: Vec<PromptMessage>,
 }
 
 pub type PromptKey = String;
 
 #[derive(Debug, Clone, Serialize, Deserialize, Deref)]
-pub struct Prompts(pub HashMap<PromptKey, Prompt>);
+pub struct Prompts(pub HashMap<PromptKey, JilebiPrompt>);
 
 impl TryFrom<&toml::Value> for Prompts {
     type Error = String;
 
     fn try_from(value: &toml::Value) -> Result<Self, Self::Error> {
-        match value {
-            toml::Value::Table(map) => {
-                let mut prompt_map: HashMap<PromptKey, Prompt> = HashMap::new();
-                for (key, table) in map.iter() {
-                    let Ok(prompt) = (*table).clone().try_into() else {
-                        return Err(format!(
-                            "Invalid format for resource definition {key}, check the toml"
-                        ));
-                    };
-                    prompt_map.insert(key.to_string(), prompt);
-                }
-                Ok(Self(prompt_map))
-            }
-            _ => Err("Invalid format for Resources section".to_string()),
+        let map = value
+            .as_table()
+            .ok_or("Invalid format for Prompts section".to_string())?;
+        let mut prompt_map: HashMap<PromptKey, JilebiPrompt> = HashMap::new();
+        for (key, table) in map.iter() {
+            let Some(op_table) = table.as_table() else {
+                return Err(format!(
+                    "Invalid format for prompt definition {key}, check the toml"
+                ));
+            };
+            let arguments: Option<Vec<PromptArgument>> = op_table
+                .get("arguments")
+                .and_then(|arg| arg.as_array())
+                .map(|args| {
+                    args.iter()
+                        .map(|argument| {
+                            let table = argument.as_table().cloned().unwrap_or_default();
+                            PromptArgument {
+                                name: mandatory_extractor(&table, key, &"name".to_string())
+                                    .unwrap_or_default(),
+                                description: optional_extractor(&table, &"description".to_string()),
+                                required: optional_extractor(&table, &"required".to_string())
+                                    .and_then(|s| s.parse().ok()),
+                            }
+                        })
+                        .collect()
+                });
+            let prompt = Prompt {
+                name: mandatory_extractor(op_table, key, &"name".to_string())?,
+                description: optional_extractor(op_table, &"description".to_string()),
+                arguments: arguments,
+            };
+            let content: Vec<PromptMessage> = op_table
+                .get("messages")
+                .and_then(|s| s.as_array())
+                .map(|messages| {
+                    messages
+                        .iter()
+                        .map(|message| {
+                            let table = message.as_table().cloned().unwrap_or_default();
+                            let role = mandatory_extractor(&table, key, &"role".to_string())
+                                .ok()
+                                .map(|role| match role.as_str() {
+                                    "assistant" => PromptMessageRole::Assistant,
+                                    _ => PromptMessageRole::User,
+                                })
+                                .unwrap_or(PromptMessageRole::User);
+                            let text = mandatory_extractor(&table, key, &"content".to_string())
+                                .unwrap_or_default();
+                            let content = PromptMessageContent::text(text);
+                            PromptMessage { role, content }
+                        })
+                        .collect()
+                })
+                .unwrap_or_default();
+            prompt_map.insert(key.to_string(), JilebiPrompt { prompt, content });
         }
+        Ok(Self(prompt_map))
     }
 }
 
